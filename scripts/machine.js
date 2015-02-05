98487f0c5d6bec00e94af49b34736279838a7434
Michael Martin <mmartin@dealersocket.com>
2015-02-04 22:22:15 -0800

    reformatting, naming "Brute" creeps to "Bruce" for Tyler

--- scripts/machine.js
+++ scripts/machine.js
@@ -11,60 +11,74 @@ var minerBrain = require('minerBrain');
 var bruteBrain = require('bruteBrain');
 var medicBrain = require('medicBrain');
 var builderBrain = require('builderBrain');
+
 module.exports = {
     chew: function (creep) {
+
         // only job is to put any spawning creep into none state
         switch (creep.memory.state) {
-            case STATE.NONE: {
-                break;
-            }
-            case STATE.SPAWNING: {
-                var found = false;
-                for (var s in Game.spawns) {
-                    if(Game.spawns[s].spawning === creep){
-                        found = true;
+            case STATE.NONE:
+                {
+                    break;
+                }
+            case STATE.SPAWNING:
+                {
+                    var found = false;
+                    for (var s in Game.spawns) {
+
+                        if (Game.spawns[s].spawning === creep) {
+                            found = true;
+                        }
                     }
+                    if (!found) {
+                        creep.memory.state = STATE.NONE;
+                    }
+                    break;
                 }
-                if(!found){
-                    creep.memory.state = STATE.NONE;
-                }
+            default:
                 break;
-            }
-            default: break;
         }
-        
+
         switch (creep.memory.role) {
-            case ROLE.MINER: {
-                minerBrain.think(creep);
-                break;
-            }
-            case ROLE.BRUTE:{
-                bruteBrain.think(creep);
-                break;
-            }
-            case ROLE.ARCHER:{
-                bruteBrain.think(creep);
-                break;
-            }
-            case ROLE.CAPTIAN:{
-                bruteBrain.think(creep);
-                break;
-            }
-            case ROLE.MEDIC:{
-                medicBrain.think(creep);
-                break;
-            }
-            case ROLE.PACKER:{
-                builderBrain.think(creep);
-                break;
-            }
-            case ROLE.WORKER:{
-                builderBrain.think(creep);
-                break;
-            }
-            default:{
-                break;
-            }
+            case ROLE.MINER:
+                {
+                    minerBrain.think(creep);
+                    break;
+                }
+            case ROLE.BRUTE:
+                {
+                    bruteBrain.think(creep);
+                    break;
+                }
+            case ROLE.ARCHER:
+                {
+                    bruteBrain.think(creep);
+                    break;
+                }
+            case ROLE.CAPTIAN:
+                {
+                    bruteBrain.think(creep);
+                    break;
+                }
+            case ROLE.MEDIC:
+                {
+                    medicBrain.think(creep);
+                    break;
+                }
+            case ROLE.PACKER:
+                {
+                    builderBrain.think(creep);
+                    break;
+                }
+            case ROLE.WORKER:
+                {
+                    builderBrain.think(creep);
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
         }
     }
 };
--- scripts/spawner.js
+++ scripts/spawner.js
@@ -5,7 +5,7 @@
  * You can import it from another modules like this:
  * var mod = require('spawner'); // -> 'a thing'
  */
-module.exports = 
+module.exports =
 {
     remember: function (creep, state, role) {
         if (!creep.memory) {
@@ -13,8 +13,7 @@ module.exports =
                 state: state,
                 role: role
             };
-        }
-        else {
+        } else {
             creep.memory.state = state;
         }
     },
@@ -38,76 +37,100 @@ module.exports =
     },
     spawn: function () {
         var result = [];
+
+        var getName = function (role) {
+            switch (role) {
+                case ROLE.BRUTE:
+                    return 'Bruce';
+                default:
+                    return role;
+            }
+        };
+
         for (var s in Game.spawns) {
+
             var spawn = Game.spawns[s];
             if (spawn.spawning === null) {
-                
-                
+
                 // find the settings that are for a population more then, but closest to what we have.
                 var weHave = spawn.room.find(Game.MY_CREEPS).length;
                 var closestDiff = 0;
                 var closestSettingsPop = null;
                 var closestSettings = null;
-                this._settings.forEach(function(setting){
+                this._settings.forEach(function (setting) {
+
                     //if(closestSettingsPop)
                     //    console.log("avail setting: " + parseInt(setting.population) + " we are: "  + parseInt(closestSettingsPop.population - weHave) + " could have: "+ parseInt(setting.population - weHave));
-                    if(closestSettingsPop && closestSettingsPop.population < weHave ){
+
+                    if (closestSettingsPop && closestSettingsPop.population < weHave) {
+
                         closestSettingsPop = setting;
                         closestSettings = setting.profile;
                         closestDiff = closestSettingsPop.population - weHave;
                         console.log("closest1 : " + parseInt(closestSettingsPop.population));
-                    } else if (!closestSettingsPop){
+                    } else if (!closestSettingsPop) {
+
                         closestSettingsPop = setting;
                         closestSettings = setting.profile;
                         closestDiff = closestSettingsPop.population - weHave;
                         console.log("closest2 : " + parseInt(closestSettingsPop.population));
                     }
                 });
+
                 // TODO: before the sort will work we need to collect the number of creeps we have in each state
                 // so check creep.memory.role and accumulate it to update how many we have.
-                
-                if(closestSettings)
-                {
-                    closestSettings.forEach(function(x){
-                        x.HAVE = spawn.room.find(Game.MY_CREEPS, {filter:function(f){
-                           // console.log('mem role: '+ f.memory.role +' other role: ' + x.ROLE)
-                            return f.memory.role == x.ROLE;
-                        }}).length;
+
+                if (closestSettings) {
+                    closestSettings.forEach(function (x) {
+
+                        x.HAVE = spawn.room.find(Game.MY_CREEPS, {
+                            filter: function (f) {
+                                // console.log('mem role: '+ f.memory.role +' other role: ' + x.ROLE)
+                                return f.memory.role == x.ROLE;
+                            }
+                        }).length;
+
                         x.TOTAL = spawn.room.find(Game.MY_CREEPS).length;
                         x.RATIO = x.HAVE / x.TOTAL;
-                        x.TARGET = x.WANT / (closestSettings.reduce(function(a, b) {return a + b.WANT;},0));
+                        x.TARGET = x.WANT / (closestSettings.reduce(function (a, b) {
+                            return a + b.WANT;
+                        }, 0));
                     });
                 }
                 // alternativly we could store state in our spawns and collect that.
-            
+
                 // if we have as many as our profile wants, then we move to a percentage strategy based off of wants.
-                
-            
+
+
                 var current = closestSettings.sort(this.comp)[0];
-                if(current.WANT <= current.HAVE){
+                if (current.WANT <= current.HAVE) {
                     current = closestSettings.sort(this.compRatio)[0];
                 }
-               if (current && current.BODY ){
-					for (var i = 0; i < current.BODY.length; i++) {
-					   var parts = current.BODY[i].parts;
-					   var id = 1;
-					   var buildCode = spawn.createCreep(parts, current.ROLE + ' ' + parseInt(spawn.room.find(Game.CREEPS).length), { state: current.STATE, role: current.ROLE });
-					   var tries = 5;
-					   while(buildCode == -3 && --tries > 0){
-						   spawn.createCreep(parts, current.ROLE + ' ' + parseInt(++id), { state: current.STATE, role: current.ROLE });
-					   }
-					   
-					   if (buildCode >= 0) {
-							result.push({
-								body: body,
-								code: buildCode,
-								have: current.HAVE,
-								role: current.ROLE
-							});
-							break;
-						}
-					}
-				}
+
+                if (current && current.BODY) {
+                    for (var i = 0; i < current.BODY.length; i++) {
+
+                        var parts = current.BODY[i].parts;
+                        var id = 1;
+                        var name = getName(current.ROLE) + ' ' + parseInt(spawn.room.find(Game.CREEPS).length);
+                        var buildCode = spawn.createCreep(parts, name, { state: current.STATE, role: current.ROLE });
+                        var tries = 5;
+                        while (buildCode == -3 && --tries > 0) {
+
+                            spawn.createCreep(parts, current.ROLE + ' ' + parseInt(++id), { state: current.STATE, role: current.ROLE });
+                        }
+
+                        if (buildCode >= 0) {
+                            result.push({
+                                body: body,
+                                code: buildCode,
+                                have: current.HAVE,
+                                role: current.ROLE
+                            });
+                            break;
+                        }
+                    }
+                }
             }
         }
         return result;
